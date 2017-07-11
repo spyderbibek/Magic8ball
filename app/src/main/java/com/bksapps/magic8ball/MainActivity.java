a918ee26abe1b02f8c14159629af29a99d83c6b4
spyderbibek <spyderbibek22@gmail.com>
2017-07-11 15:48:58 +0545

    Fixed Out of Index Error

--- app/src/main/java/com/bksapps/magic8ball/MainActivity.java
+++ app/src/main/java/com/bksapps/magic8ball/MainActivity.java
@@ -2,6 +2,12 @@ package com.bksapps.magic8ball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button askButton=(Button)findViewById(R.id.askButton);
+        final ImageView image_ball= (ImageView)findViewById(R.id.image_ball);
+        final int[] ballArray={
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,
+        };
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand=new Random();
+                int number=rand.nextInt(5);
+                Log.i("Info","Random Number is: "+ number);
+                image_ball.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
